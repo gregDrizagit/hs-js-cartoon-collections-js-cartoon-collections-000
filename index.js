fe385075fdd6a010ea752fe83419f9e91ef5f60a
Greg Driza <gdriza@uarts.edu>
2017-09-13 15:19:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function dwarfRollCall(dwarves)
   for(var i = 1; i < dwarves.length - 1; i++)
   {
 
-    dwarfString = `${i}. ${dwarves[i - 1]}`;
+    dwarfString += `${i}. ${dwarves[i - 1]}`;
 
   }
   return dwarfString; 
